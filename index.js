ff5dd249c18501d75b34918b498e73deb6043305
corbanrwi <rwigambacorban@gmail.com>
2026-09-11 23:24:47 +0200

    initial committ

---
+++ index.js
@@ -0,0 +1,17 @@
+// const name = "corban is big bro"; variable
+// console.log("hello",name)
+// ;
+// let age = 20;
+// age = 21;
+// console.log(age);
+
+//  const name = ["corban", "is", "big", "bro"]; array
+//  console.log(name[0]);
+
+// const user = {name: "corban",age: 20, } object 
+
+function greet(name){
+    return "hello" + " " + name;
+}
+   
+console.log(greet('corban'));
